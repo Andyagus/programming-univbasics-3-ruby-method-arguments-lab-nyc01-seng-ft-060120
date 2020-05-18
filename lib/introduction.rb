88e6a3991056dceb76f6945bbefbe04330ee2e01
Andy Agus <redagus@gmail.com>
2020-05-18 01:33:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,5 @@ def
 end
 
 def introduction_with_language_optional (name, language = "ruby")
-  puts  "Hi, my name is #{name} and I am learning to program in #{language}.
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
